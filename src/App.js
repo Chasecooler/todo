9dd4ca1b84426fbf0d73c7673219a175b69cb528
Chasecooler <chasemarq@yahoo.com>
2019-07-15 17:14:51 -0400

    todo update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, from 'react';
+import { SSL_OP_LEGACY_SERVER_CONNECT } from 'constants';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      currentToDo: "",
+    }
+  }
+
+handleChange = event => {
+  console.log(event.target.value);
+
 }
 
-export default App;
+addItem = event => {
+  event.preventDefault();
+  console.log("addItem Method fired");
+}
+
+render() {
+  return(
+    <div clasName="container">
+      <form onSubmit={this.addItem}>
+        <label htmlFor="taskName">Task Name:</label>
+        input onChange={this.handleChange} name="taskName" type="text" placeholder="Add ToDo Here!" />
+        <button type="submit">Add Task</button>
+
+      </form>
+    </div>
+  )
+}
+
+}
+
+
+
+export default TodoList
+
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
